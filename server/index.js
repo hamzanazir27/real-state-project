2f2da8f742ae77e247e4bc304853091c8806eb0e
hamzanazir27 <hamzaramay51@gmail.com>
2025-08-31 22:31:42 +0500

    created test api

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js"; // extension add karo
+
 const app = express();
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("db connected"))
   .catch((e) => console.log("error", e.message));
 
-const PORT = process.env.PORT;
+app.use("/api/users", userRoute);
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
