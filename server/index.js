369bef633309ccbb532e14391665cfc2f2ba0a54
hamzanazir27 <hamzaramay51@gmail.com>
2025-08-31 21:54:19 +0500

    mongoose connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mern-realstate")
+  .then(() => console.log("db connected"))
+  .catch((e) => console.log("error", e.message));
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
