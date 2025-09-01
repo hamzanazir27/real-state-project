212d65c1f47e6f9362b3af8de4ba83593c196427
hamzanazir27 <hamzaramay51@gmail.com>
2025-09-01 23:14:45 +0500

    create signup api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/errors.js";
-
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 async function signup(req, res, next) {
   const { username, email, password } = req.body;
 
@@ -33,4 +35,24 @@ async function signup(req, res, next) {
   }
 }
 
-export default { signup };
+async function signin(req, res, next) {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) next(errorHandler(404, "Invalid Email"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) next(errorHandler(404, "Invalid Craditial"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    console.log();
+    const { password: hashedPassword, ...userData } = validUser._doc;
+    res
+      .cookie("acess-token", token, { httpOnly: true })
+      .status(200)
+      .json(userData);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export default { signup, signin };
