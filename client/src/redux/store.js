90893a45c47826d7bf81b2292886ba46053d65ff
hamzanazir27 <hamzaramay51@gmail.com>
2025-09-03 21:11:21 +0500

    redux persist user for user saved in local storage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+const rootReducer = combineReducers({ user: userReducer });
+import { persistStore, persistReducer } from "redux-persist";
+
+import storage from "redux-persist/lib/storage"; // defaults to localStorage
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
