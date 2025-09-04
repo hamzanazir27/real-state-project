6eefbfe44e8f468a8579dde48f23b114175de79a
hamzanazir27 <hamzaramay51@gmail.com>
2025-09-04 17:34:19 +0500

    added prtected rpute only sign in user see profile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 function Header() {
+  const user = useSelector((state) => state.user.currentUser);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -20,7 +21,7 @@ function Header() {
           <FaSearch className="text-slate-500" />
         </form>
 
-        <ul className="flex gap-4">
+        <ul className="flex gap-4 items-center">
           <Link to="/">
             <li className="hidden sm:inline text-slate-700 hover:underline">
               Home
@@ -31,8 +32,12 @@ function Header() {
               About
             </li>
           </Link>
-          <Link to="/signin">
-            <li className="text-slate-700 hover:underline">Sign in</li>
+          <Link to="/profile">
+            {user ? (
+              <img className="rounded-3xl w-10 h-10 " src={user.avatar} />
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
