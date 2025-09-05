df4e78f4a070bf244a3da3e090a6b8b768d5d68b
hamzanazir27 <hamzaramay51@gmail.com>
2025-09-05 16:28:21 +0500

    add image to cloudnary

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,6 +9,7 @@ function Header() {
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Ubanto</span>
+
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
