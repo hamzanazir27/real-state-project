5ffdf04c3f4cf0203e956825eb68c3d03bab83e1
hamzanazir27 <hamzaramay51@gmail.com>
2025-09-12 17:23:50 +0500

    crearte update listing api route

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -3,6 +3,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../controllers/listing.controller.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 // POST /api/listing/create
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.patch("/update/:id", verifyToken, updateListing);
 
 export default router;
