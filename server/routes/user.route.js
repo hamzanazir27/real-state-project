acd19ad837d88bc11dd41dd06da794983b889b71
hamzanazir27 <hamzaramay51@gmail.com>
2025-09-12 14:44:30 +0500

    create get all listing by users

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,10 +3,11 @@ import express from "express";
 import userControllers from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
-const { test, updateUser, deleteUser } = userControllers;
+const { test, updateUser, deleteUser, getUserListing } = userControllers;
 const router = express.Router();
 
 router.get("/test", test);
 router.patch("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listing/:id", verifyToken, getUserListing);
 export default router;
